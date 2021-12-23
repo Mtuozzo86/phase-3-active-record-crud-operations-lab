e4ad382b73fc5f90a3dab9dc1ac925e6d125fd07
Mtuozzo86 <mtuozzo86@gmail.com>
2021-12-23 11:42:01 -0500

    commit tree 0b7cef93899ca5dc5feecbac1e4660335e30cdc1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,30 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+
+    def self.first_movie
+        self.first
+    end
+
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def Movie.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(attr)
+        self.find_by(attr)
+    end
+
+    def self.find_movies_after_2002
+        self.where("release_date > 2002")
+    end
 
 end
